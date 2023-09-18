c9474722bb5a51b326a09aaeea163921dc3b3368
Sunil A Kumar <Sunil.A.Kumar@Geminisolutions.com>
2023-09-18 14:49:57 +0530

    merged head 3 commit message to one

--- test_git.py
+++ test_git.py
@@ -1,4 +1,7 @@
 this is a git practice session. 
 opened again as said.
 this changes are made in feature branch.😊
-now making changes in main branch.`
+now making changes in main branch.
+making changes to see git diff`
+test2
+test3
