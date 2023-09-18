3760efd131f4a47577048db802111b61459f022a
Sunil A Kumar <Sunil.A.Kumar@Geminisolutions.com>
2023-09-18 14:37:06 +0530

    direct push to remote

--- test_git.py
+++ test_git.py
@@ -1,3 +1,4 @@
 this is a git practice session. 
 opened again as said.
-this changes are made in feature branch.😊`
+this changes are made in feature branch.😊
+again making changes in feature branch to directly push to remote repo`
